c2a5d1104d976b65f8706c8df59a5393e96ffb17
Matt Kubilus <mattkubilus@gmail.com>
2020-04-11 16:44:11 -0400

    Add doc

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.8'
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
+
+VERSION = '0.0.9'
 
 setup(
     name="mkdocs-rebalance-theme",
@@ -8,6 +14,8 @@ setup(
     url='https://github.com/kubilus1/mkdocs-rebalance-theme',
     license='GPL',
     description='Rebalance theme for mkdocs',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Matt Kubilus',
     author_email='mattkubilus@gmail.com',
     packages=find_packages(),
