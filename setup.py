802f9b6915e0a6324a8837d0e02299c0f2ddb13d
Matt Kubilus <mattkubilus@gmail.com>
2023-08-13 16:27:32 -0400

    Bump ver

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
         long_description = f.read()
 
-VERSION = '0.0.15'
+VERSION = '0.0.16'
 
 setup(
     name="mkdocs-rebalance-theme",
